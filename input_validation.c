9cd39e06516507c587b2f55f48208ce043a3c58f
cs23b1086-gif <cs23b1086@iiitdm.ac.in>
2026-03-09 21:04:56 +0530

    Create input_validation.c

---
+++ input_validation.c
@@ -0,0 +1,36 @@
+/*
+Hint for students
+
+The program assumes all input is valid.
+Add validation checks.*/
+
+#include <stdio.h>
+
+int main()
+{
+    int n;
+    int marks[100];
+    int i;
+
+    printf("Enter number of students: ");
+    scanf("%d", &n);
+
+    for(i = 0; i < n; i++)
+    {
+        printf("Enter marks: ");
+        scanf("%d", &marks[i]);
+    }
+
+    int sum = 0;
+
+    for(i = 0; i < n; i++)
+    {
+        sum = sum + marks[i];
+    }
+
+    int avg = sum / n;
+
+    printf("Average marks: %d\n", avg);
+
+    return 0;
+}
